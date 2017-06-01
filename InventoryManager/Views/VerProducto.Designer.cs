fae1d7b5317a1515333239c2bb18e5b9dfd3c02a
zs10011598 <zs10011598@gmail.com>
2017-06-01 13:00:49 -0500

    be humble sit down

--- InventoryManager/Views/VerProducto.Designer.cs
+++ InventoryManager/Views/VerProducto.Designer.cs
@@ -28,11 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(24, 47);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(391, 95);
+            this.textBox1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(438, 21);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(140, 121);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // VerProducto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(602, 285);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "VerProducto";
             this.Text = "VerProducto";
+            this.Load += new System.EventHandler(this.VerProducto_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
--- InventoryManager/Views/VerProducto.cs
+++ InventoryManager/Views/VerProducto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using InventoryManager.DataAccess;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,10 +13,28 @@ namespace InventoryManager.Views
 {
     public partial class VerProducto : Form
     {
+
+        string product;
         public VerProducto(string producto)
         {
             // productos: "camisa","blusa","pantalon-hombre","pantalon-mujer","accesirio-hombre","accesorio-mujer","saco","vestido"
             InitializeComponent();
+            product = producto;
+        }
+
+        private void VerProducto_Load(object sender, EventArgs e)
+        {
+           
+            label1.Text = product;
+            bdManager dbm = new bdManager();
+            textBox1.Text = "Descripción: " + dbm.description(product);
+            textBox1.ReadOnly = true;
+            Bitmap image = new Bitmap(InventoryManager.Properties.Resources.d);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
